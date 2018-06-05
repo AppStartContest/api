2301ec4966606b8ed98b5d0808974a4ef7e7c718
RobinMarechal <robin.marechal-2@etu.univ-tours.fr>
2018-06-05 04:21:34 +0200

    Fixed typo in App\Event#fillable

--- app/Event.php
+++ app/Event.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Event extends Model
 {
 	use SoftDeletes;
-	protected $fillable = ['room_id', 'name', 'data', 'time', 'description'];
+	protected $fillable = ['room_id', 'name', 'date', 'time', 'description'];
 
 	public function messages(){
 		return $this->hasMany('App\Message');
